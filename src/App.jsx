555942b70fe409e05308afb0de759033af39e58c
try <dlsduf@gmail.com>
2022-03-03 13:47:30 +0900

    [BoilerPlate] 전체 wrapper 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 import Main from './pages/Main';
+import styled from 'styled-component';
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Wrapper>
+          <Route path="/" element={<Main />} />
+        </Wrapper>
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  width: 360px;
+  height: 812px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
