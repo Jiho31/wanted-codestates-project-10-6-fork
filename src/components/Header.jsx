2686d96e583bc1560aea060ff9cd12ea0433d3b9
Jiho Bok <bok.jiho@gmail.com>
2022-03-03 20:56:54 +0900

    [Style] 뒤로가기 화살표 아이콘 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,7 @@ import styled from 'styled-components';
 const Header = () => {
   return (
     <HeaderComponent>
-      <button>
-        <svg
-          width="24px"
-          height="24px"
-          viewBox="0 0 48 48"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M31 36L19 24L31 12"
-            stroke="black"
-            stroke-width="4"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </button>
+      <button>&#xE000;</button>
       <p>돌보미 신청하기</p>
     </HeaderComponent>
   );
@@ -35,9 +19,12 @@ const HeaderComponent = styled.header`
   padding: 0 16px;
 
   button {
+    color: #5b5555;
     cursor: pointer;
     width: 24px;
     height: 24px;
+    font-size: 22px;
+    font-weight: 500;
   }
   p {
     width: 296px;
