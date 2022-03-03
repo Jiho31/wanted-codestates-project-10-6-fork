e455806a5ea38a42256a3f62e94c2e8996a1a7d9
Space-Belt <dngurwn@gmail.com>
2022-03-04 01:00:01 +0900

    [FIX] 수정

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,9 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
 import page from './page';
 //! 한프로젝트에 리듀서가 여러개일경우 한 리듀서로 합쳐사용한다.
 //! 합쳐진 리듀서를 루트 리듀서라고 부른다.
 
-const rootReducer = combineReducers({ counter, page });
+const rootReducer = combineReducers({ page });
 
 export default rootReducer;
 
