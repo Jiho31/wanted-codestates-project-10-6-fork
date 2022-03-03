9e27b6fb17edc165c5afe7ab6e91c72fed0343e4
juo <hwgiwh271@gmail.com>
2022-03-03 16:50:38 +0900

    [Feat] #11 돌봄 유형 컴포넌트 UI구현
    -

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import SelectBox from 'components/SelectBox';
+import styled from 'styled-components';
+
 export default function Apply() {
-  return <div>apply</div>;
+  return (
+    <div>
+      apply
+      <BoxWrapper>
+        <SelectBox icon="🌞">24시간 상주</SelectBox>
+        <SelectBox icon="⏰">시간제 돌봄</SelectBox>
+      </BoxWrapper>
+    </div>
+  );
 }
+const BoxWrapper = styled.div`
+  display: flex;
+`;
