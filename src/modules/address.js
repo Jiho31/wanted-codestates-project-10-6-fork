509dba8565a08c4e655bc9c8abc735f957493b9e
가즈아 <wjsdydxo45@naver.com>
2022-03-04 18:28:18 +0900

    [Feat] #3 돌봄주소 - 컴포넌트 view작업 / 주소검색 API연동 / 주소검색 기능 구현

--- src/modules/address.js
+++ src/modules/address.js
@@ -1,13 +1,28 @@
 const SET_ADDRESS = 'address/SET_ADDRESS';
+const USER_ADDRESS = 'address/USER_ADDRESS';
 
 export const setAddress = (addr) => ({ type: SET_ADDRESS, addr });
+export const setUserAddress = (addr) => ({ type: USER_ADDRESS, addr });
 
-const initialState = '';
+const initialState = {
+  roadAddr: null,
+  jibunAddr: null,
+  userAddr: null,
+};
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_ADDRESS:
-      return action.addr;
+      return {
+        ...state,
+        roadAddr: action.addr.roadAddr,
+        jibunAddr: action.addr.jibunAddr,
+      };
+    case USER_ADDRESS:
+      return {
+        ...state,
+        userAddr: action.addr,
+      };
     default:
       return state;
   }
